25c50ac222bcd71cab84b2c262d0d03426789d09
Shamim <sahmed56@hotmail.com>
2020-08-18 22:41:24 -0400

    Update main.tf

--- terraform/dev-project-name/us-east-1/main.tf
+++ terraform/dev-project-name/us-east-1/main.tf
@@ -15,14 +15,14 @@ provider "aws" {
   region  = module.conf.region
 }
 
-# resource "aws_instance" "example" {
-#   ami           = "ami-0ff8a91507f77f867"
+resource "aws_instance" "example" {
+   ami           = "ami-0ff8a91507f77f867"
+   instance_type = module.conf.instance_type
+}
+
+# module "dev-instance" {
+#   source = "../../modules/instance/"
 #   instance_type = module.conf.instance_type
 # }
-
-module "dev-instance" {
-  source = "../../modules/instance/"
-  instance_type = module.conf.instance_type
-}
 
 
